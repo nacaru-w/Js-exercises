a1b40eec9602c690f970a8f1fcb7b1df323e1837
nasch92 <ignaciocr92@gmail.com>
2022-12-13 13:35:21 +0100

    Setting template for week 11 exercises

---
+++ Ejercicios Semana 11/Resolución ejercicios semana 11.js
@@ -0,0 +1,23 @@
+// 1- Crea un algoritmo que calcule la media de 5 números que se le pasan por teclado. Sólo se podrán utilizar bucles.
+
+
+// 2- Utilizando bucles, genera dinámicamente la siguiente serie:
+// 1, 5, 3, 7, 5, 9, 7, ..., 23.
+// 23 será el último dígito de la serie.
+
+
+// 3- Crear un código que utilizando un bucle dibuje la secuencia de Fibonacci del valor n.
+// La secuencia de Fibonacci consiste en la suma de los dos números anteriores, partiendo de 1, 1, 2, 3, 5, 8, 13, 21...
+// Por ejemplo:
+// la secuencia para n=5 de Fibonacci será: 1, 1, 2, 3, 5
+// la secuencia para n=7 de Fibonacci será: 1, 1, 2, 3, 5, 8, 13
+
+
+// 4- Solicitar al usuario que ingrese una frase y luego imprimir la cantidad de vocales que se encuentran en dicha frase.
+// Por ejemplo:
+// - "El martes tengo que ver a mi prima" devolverá 12
+// - "No sé si tengo que ir" devolverá 8
+
+
+// 5- Crear un programa que muestre todos los números que son múltiplos de 3 en una única lista concatenada de la siguiente forma:
+// 3, 6, 9, 12, ...

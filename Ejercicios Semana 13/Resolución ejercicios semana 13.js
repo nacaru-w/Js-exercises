c11c5fe7db7822ea04d5001173f508b1523ea6d4
nasch92 <ignaciocr92@gmail.com>
2022-12-28 11:42:02 +0100

    Adding template for exercises

---
+++ Ejercicios Semana 13/Resolución ejercicios semana 13.js
@@ -0,0 +1,19 @@
+// 1- Crear un código que devuelva un carácter de un string a partir de su posición (índice en el String).
+
+// 2- Crear un código que concatene dos Strings
+
+// 3- Dado un Array que contenga todo letras, que cree un String con el mismo orden que el array
+
+// 4- Crear un código que devuelva si un String termina con un String dado.
+
+// 5- Crear una función que compare dos Strings y nos diga si son iguales.
+
+// 6- Transformar un String en un Array en el que cada posición contenga cada una de las letras del String y después volver a construir un Array con el contenido del String pero en orden inverso.
+
+// 7- Imprimir por pantalla la fecha actual
+
+// 8- Crear una fecha con los siguientes datos: 20 de mayo de 2018
+
+// 9- Crear un código que imprima el número de milisegundos trascurridos desde el 1 de enero de 1970 a las 00.00.00 hasta el momento donde se ejecute tu código.
+
+// 10- Dado un numero introducido por el usuario, comprobar si es o no es un número entero.
